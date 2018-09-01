9a34cab7ec0f95043c6279ba156dc2b21ce0a548
Metaron <wwzzyyzzrr@gmail.com>
2018-09-01 18:38:56 +0800

    Finsh recognition path

--- venv/path_2.py
+++ venv/path_2.py
@@ -1,10 +1,12 @@
+# -*- coding:utf8 -*-
+
 import math
 
 
 class coordinate:
     def __init__(self):
-        latitude = float(0.0)#纬度
-        longitude = float(0.0)#经度
+        latitude = float(0.0) #纬度
+        longitude = float(0.0) #经度
         height = float(0.0)
 
 def the_first_point(a1, a2, a3):
@@ -73,12 +75,85 @@ def second_O(p1,p2,p3):
 def direction(p1,p2):
     k = p1.latitude / p1.longitude
     if(p2.latitude > p2.longitude * k):#在上方
-        i = 1
-    else:
         i = -1
+    else:
+        i = 1
     if(p2.longitude > 0):#在一四象限
-        i *= -1
-    return i#i为1则顺时针,为-1则逆时针
+        i *= 1
+    return i#i为-1则顺时针,为1则逆时针
+
+'''直线po的斜率角'''
+def get_angle(p,o):
+    p.longitude -= o.longitude
+    p.latitude -= o.latitude
+    p.height -= o.height
+    k = p.latitude / p.longitude
+    angle = math.atan(k)
+    if (k < 0):
+        if(p.latitude > 0):
+            angle += math.pi
+        else:
+            angle += math.pi * 2
+    else:
+        if(p.latitude < 0):
+            angle += math.pi
+    return angle
+
+'''第一个圆弧轨道'''
+def first_circle(p1,p2,n):
+    o = first_O(p1, p2)
+    r = pow(pow(o.latitude, 2) + pow(o.longitude, 2), 0.5)
+    d = direction(p1, p2)
+    a1 = get_angle(p1, o)
+    a2 = get_angle(p2, o)
+    if (d == -1):
+        if (a2 > a1):
+            a2 -= 2 * math.pi
+    else:
+        if (a1 > a2):
+            a1 -= 2 * math.pi
+    a_i = math.fabs(a1 - a2) / n
+    h_i = (p2.height - p1.height) / n
+    path = []
+    temp = coordinate()
+    for i in range(0,n):
+        temp.longitude = r * math.cos(a1 + a_i * i * d) + o.longitude
+        temp.latitude = r * math.sin(a1 + a_i * i * d) + o.latitude
+        temp.height = p1.height + h_i * i
+        path.append(temp)
+    return path
+
+'''直线轨道'''
+def the_line(p1,p2,n):
+    x_i = (p2.longitude - p1.longitude) / n
+    y_i = (p2.latitude -p1.latitude) / n
+    h_i = (p2.height - p1.height) / n
+    path = []
+    temp = coordinate()
+    for i in range(0,n):
+        temp.longitude = p1.longitude + x_i * i
+        temp.longitude = p1.latitude + y_i * i
+        temp.height = p1.height + h_i * i
+        path.append(temp)
+    return path
+
+'''第二个圆弧轨道'''
+def second_circle(p1,p2,p3,n):
+    o = second_O(p1,p2,p3)
+    r = pow(pow(o.latitude - p2.latitude, 2) + pow(o.longitude - p2.longitude, 2), 0.5)
+    d = -1 * direction(relative_coordinate(p2,p3),relative_coordinate(p1,p3))
+    a1 = get_angle(p2,o)
+    a2 = a1 + d * 2 * math.pi
+    a_i = math.pi * 2 / n
+    h_i =(p3.height - p2.height) / n
+    path = []
+    temp = coordinate()
+    for i in range(0,n):
+        temp.longitude = r * math.cos(a1 + a_i * i * d) + o.longitude
+        temp.latitude = r * math.sin(a1 + a_i * i * d) + o.latitude
+        temp.height = p1.height + h_i * i
+        path.append(temp)
+    return path
 
 def path(x):
     p1 = x[0]
@@ -90,8 +165,17 @@ def path(x):
         temp_p = p3
         p3 = p2
         p2 = temp_p
-
-
+    o1 = first_O(p1,p2)
+    r1 = pow(pow(o1.latitude,2)+pow(o1.longitude,2),0.5)
+    d = direction(p1,p2)
+    a1_c1 = get_angle(p1,o)
+    a2_c1 = get_angle(p2,o)
+    if (d == 1):
+        if(a2_c1 > a1_c1):
+            a2_c1 -= 2 * math.pi
+    else:
+        if(a1_c1 > a2_c1):
+            a1_c1 -= 2 * math.pi
 
 a1 = coordinate()
 a2 = coordinate()
@@ -109,6 +193,8 @@ a2.height = float(20.0)
 a3.longitude = float(5.0)
 a3.latitude = float(4.0)
 a3.height = float(20.0)
+
+print math.atan(-1)
 
 
 
