aede1f7d9824d394a4698104c9cb6abde1710e28
Metaron <wwzzyyzzrr@gmail.com>
2018-09-02 22:12:46 +0800

    Finsh the first versions

--- venv/path_2.py
+++ venv/path_2.py
@@ -39,22 +39,20 @@ def show_coordinate(a):
 
 '''获取相对坐标,即以o点为圆心,p的坐标'''
 def relative_coordinate(p,o):
-    p.latitude -= o.latitude
-    p.longitude -= o.longitude
-    p.height -= o.height
-    return p
+    p_t = coordinate()
+    p_t.latitude = p.latitude - o.latitude
+    p_t.longitude = p.longitude - o.longitude
+    p_t.height = p.height - o.height
+    return p_t
 
 '''第一个圆弧轨道的圆心'''
 def first_O(p1,p2):
     k1 =  (p1.longitude * -1.0) / (p1.latitude * 1.0)
     k2 = ((p2.longitude - p1.longitude) * -1.0) / ((p2.latitude - p1.latitude) * 1.0)
-    print k1
-    print k2
     a1 = p1.longitude * 1.0 / 2
     b1 = p1.latitude * 1.0 / 2
-    a2 = p2.longitude
-    b2 = p2.latitude
-    print "%f %f %f %f"%(a1,b1,a2,b2)
+    a2 = p1.longitude
+    b2 = p1.latitude
     O = coordinate()
     O.longitude = (b1 - b2 + k2 * a2 - k1 * a1) / (k2 - k1)
     O.latitude = k1 * (O.longitude - a1) + b1
@@ -63,9 +61,9 @@ def first_O(p1,p2):
 
 '''第二个圆弧轨道的圆心'''
 def second_O(p1,p2,p3):
-    p1 = relative_coordinate(p1,p3)
-    p2 = relative_coordinate(p2,p3)
-    o = first_O(p2,p1)
+    p1_t = relative_coordinate(p1,p3)
+    p2_t = relative_coordinate(p2,p3)
+    o = first_O(p2_t,p1_t)
     o.latitude += p3.latitude
     o.longitude += p3.longitude
     o.height += p3.height
@@ -79,23 +77,24 @@ def direction(p1,p2):
     else:
         i = 1
     if(p2.longitude > 0):#在一四象限
-        i *= 1
+        i *= -1
     return i#i为-1则顺时针,为1则逆时针
 
-'''直线po的斜率角'''
+'''直线po的圆心角'''
 def get_angle(p,o):
-    p.longitude -= o.longitude
-    p.latitude -= o.latitude
-    p.height -= o.height
-    k = p.latitude / p.longitude
+    p_t = coordinate()
+    p_t.longitude = p.longitude - o.longitude
+    p_t.latitude = p.latitude - o.latitude
+    p_t.height = p.height - o.height
+    k = p_t.latitude / p_t.longitude
     angle = math.atan(k)
     if (k < 0):
-        if(p.latitude > 0):
+        if(p_t.latitude > 0):
             angle += math.pi
         else:
             angle += math.pi * 2
     else:
-        if(p.latitude < 0):
+        if(p_t.latitude < 0):
             angle += math.pi
     return angle
 
@@ -104,7 +103,65 @@ def first_circle(p1,p2,n):
     o = first_O(p1, p2)
     r = pow(pow(o.latitude, 2) + pow(o.longitude, 2), 0.5)
     d = direction(p1, p2)
-    a1 = get_angle(p1, o)
+    O = coordinate()
+    O.latitude = 0
+    O.longitude = 0
+    O.height = 0
+    a1 = get_angle(O, o)
+    a2 = get_angle(p1, o)
+    if (d == -1):
+        if (a2 > a1):
+            a2 -= 2 * math.pi
+    else:
+        if (a1 > a2):
+            a1 -= 2 * math.pi
+    a_i = math.fabs(a1 - a2) / n
+    h_i = (p2.height - p1.height) / n
+    path = []
+    for i in range(0,n):
+        path.append(coordinate())
+        path[i].longitude = r * math.cos(a1 + a_i * i * d) + o.longitude
+        path[i].latitude = r * math.sin(a1 + a_i * i * d) + o.latitude
+        path[i].height = p1.height + h_i * i
+    return path
+
+'''直线轨道'''
+def the_line(p1,p2,n):
+    x_i = (p2.longitude - p1.longitude) / n
+    y_i = (p2.latitude -p1.latitude) / n
+    h_i = (p2.height - p1.height) / n
+    path = []
+    for i in range(0,n):
+        path.append(coordinate())
+        path[i].longitude = p1.longitude + x_i * i
+        path[i].latitude = p1.latitude + y_i * i
+        path[i].height = p1.height + h_i * i
+    return path
+
+'''第二个圆弧轨道'''
+def second_circle(p1,p2,p3,n):
+    o = second_O(p1,p2,p3)
+    r = pow(pow(o.latitude - p2.latitude, 2) + pow(o.longitude - p2.longitude, 2), 0.5)
+    d = -1 * direction(relative_coordinate(p2,p3),relative_coordinate(p1,p3))
+    a1 = get_angle(p2,o)
+    a_i = math.pi * 2 / n
+    h_i =(p3.height - p2.height) / n
+    path = []
+    for i in range(0,n):
+        path.append(coordinate())
+        path[i].longitude = r * math.cos(a1 + a_i * i * d) + o.longitude
+        path[i].latitude = r * math.sin(a1 + a_i * i * d) + o.latitude
+        path[i].height = p1.height + h_i * i
+    return path
+
+def boom_second(p1,p2,p3,n):
+    return second_circle(p1,p2,p3,n)
+
+def boom_third(p1,p2,p3,n):
+    o = second_O(p1, p2, p3)
+    r = pow(pow(o.latitude - p2.latitude, 2) + pow(o.longitude - p2.longitude, 2), 0.5)
+    d = -1 * direction(relative_coordinate(p2, p3), relative_coordinate(p1, p3))
+    a1 = get_angle(p2, o)
     a2 = get_angle(p2, o)
     if (d == -1):
         if (a2 > a1):
@@ -116,46 +173,14 @@ def first_circle(p1,p2,n):
     h_i = (p2.height - p1.height) / n
     path = []
     temp = coordinate()
-    for i in range(0,n):
-        temp.longitude = r * math.cos(a1 + a_i * i * d) + o.longitude
-        temp.latitude = r * math.sin(a1 + a_i * i * d) + o.latitude
-        temp.height = p1.height + h_i * i
-        path.append(temp)
+    for i in range(0, n):
+        path.append(coordinate())
+        path[i].longitude = r * math.cos(a1 + a_i * i * d) + o.longitude
+        path[i].latitude = r * math.sin(a1 + a_i * i * d) + o.latitude
+        path[i].height = p1.height + h_i * i
     return path
 
-'''直线轨道'''
-def the_line(p1,p2,n):
-    x_i = (p2.longitude - p1.longitude) / n
-    y_i = (p2.latitude -p1.latitude) / n
-    h_i = (p2.height - p1.height) / n
-    path = []
-    temp = coordinate()
-    for i in range(0,n):
-        temp.longitude = p1.longitude + x_i * i
-        temp.longitude = p1.latitude + y_i * i
-        temp.height = p1.height + h_i * i
-        path.append(temp)
-    return path
-
-'''第二个圆弧轨道'''
-def second_circle(p1,p2,p3,n):
-    o = second_O(p1,p2,p3)
-    r = pow(pow(o.latitude - p2.latitude, 2) + pow(o.longitude - p2.longitude, 2), 0.5)
-    d = -1 * direction(relative_coordinate(p2,p3),relative_coordinate(p1,p3))
-    a1 = get_angle(p2,o)
-    a2 = a1 + d * 2 * math.pi
-    a_i = math.pi * 2 / n
-    h_i =(p3.height - p2.height) / n
-    path = []
-    temp = coordinate()
-    for i in range(0,n):
-        temp.longitude = r * math.cos(a1 + a_i * i * d) + o.longitude
-        temp.latitude = r * math.sin(a1 + a_i * i * d) + o.latitude
-        temp.height = p1.height + h_i * i
-        path.append(temp)
-    return path
-
-def path(x):
+def set_path(x):
     p1 = x[0]
     p2 = x[1]
     p3 = x[2]
@@ -165,17 +190,16 @@ def path(x):
         temp_p = p3
         p3 = p2
         p2 = temp_p
-    o1 = first_O(p1,p2)
-    r1 = pow(pow(o1.latitude,2)+pow(o1.longitude,2),0.5)
-    d = direction(p1,p2)
-    a1_c1 = get_angle(p1,o)
-    a2_c1 = get_angle(p2,o)
-    if (d == 1):
-        if(a2_c1 > a1_c1):
-            a2_c1 -= 2 * math.pi
-    else:
-        if(a1_c1 > a2_c1):
-            a1_c1 -= 2 * math.pi
+    print "第一个"
+    for i in first_circle(p1,p2,50):
+        show_coordinate(i)
+    print "直线"
+    for j in the_line(p1,p2,50):
+        show_coordinate(j)
+    print "第二个"
+    for k in second_circle(p1,p2,p3,50):
+        show_coordinate(k)
+
 
 a1 = coordinate()
 a2 = coordinate()
@@ -194,7 +218,8 @@ a3.longitude = float(5.0)
 a3.latitude = float(4.0)
 a3.height = float(20.0)
 
-print math.atan(-1)
+x = [a1,a2,a3]
+set_path(x)
 
 
 
