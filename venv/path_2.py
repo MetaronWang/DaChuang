e9b87065f07b786d38ffbf1bf1c0344d090185bf
Metaron <wwzzyyzzrr@gmail.com>
2018-09-12 12:42:49 +0800

    部分备注

--- venv/path_2.py
+++ venv/path_2.py
@@ -1,7 +1,12 @@
 # -*- coding:utf8 -*-
 
 import math
-
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import numpy as np
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
 
 class coordinate:
     def __init__(self):
@@ -116,13 +121,13 @@ def first_circle(p1,p2,n):
         if (a1 > a2):
             a1 -= 2 * math.pi
     a_i = math.fabs(a1 - a2) / n
-    h_i = (p2.height - p1.height) / n
+    h_i = ( p1.height) / n
     path = []
     for i in range(0,n):
         path.append(coordinate())
         path[i].longitude = r * math.cos(a1 + a_i * i * d) + o.longitude
         path[i].latitude = r * math.sin(a1 + a_i * i * d) + o.latitude
-        path[i].height = p1.height + h_i * i
+        path[i].height = h_i * i
     return path
 
 '''直线轨道'''
@@ -189,11 +194,11 @@ def set_path(x):
         temp_p = p3
         p3 = p2
         p2 = temp_p
-    return first_circle(p1,p2,50)+the_line(p1,p2,50)+second_circle(p1,p2,p3,50)
+    return first_circle(p1,p2,500)+the_line(p1,p2,500)+second_circle(p1,p2,p3,500)
 
 '''输入坐标点之后，调用该方法，返回识别的路径'''
 def main(o,a1,a2,a3):
-    return set_path([relative_coordinate(a1,o),relative_coordinate(a2,0),relative_coordinate(a3,o)])
+    return set_path(the_first_point(relative_coordinate(a1,o),relative_coordinate(a2,o),relative_coordinate(a3,o)))
 
 def get_GPS(p,o):
     p_t = coordinate()
@@ -202,22 +207,37 @@ def get_GPS(p,o):
     p_t.height = p.height + o.height
     return p_t
 
+def d3_points(x_list):
+    #x_list = [[3,3,2],[4,3,1],[1,2,3],[1,1,2],[2,1,2]]
+    plt.rcParams['figure.figsize'] = (60.0, 40.0)
+    plt.rcParams['image.interpolation'] = 'nearest'
+    fig = plt.figure() # 得到画面
+    ax = fig.gca(projection='3d') # 得到3d坐标的图
+    # 画点
+    for x in x_list:
+        ax.scatter(x[0],x[1],x[2],c='r')
+    plt.savefig("d3_image.png",dpi=100)
+    plt.show()
+
+
+
+
 '''这里是举个例子，如何输入坐标点'''
 a1 = coordinate()
 a2 = coordinate()
 a3 = coordinate()
 o = coordinate()
 
-a1.longitude = float(2.0)
-a1.latitude = float(3.0)
+a1.longitude = float(12223.0)
+a1.latitude = float(3334.0)
 a1.height = float(20.0)
 
-a2.longitude = float(4.0)
-a2.latitude = float(1.0)
+a2.longitude = float(33144.0)
+a2.latitude = float(515.0)
 a2.height = float(20.0)
 
-a3.longitude = float(5.0)
-a3.latitude = float(4.0)
+a3.longitude = float(6256.0)
+a3.latitude = float(3946.0)
 a3.height = float(20.0)
 
 o.longitude = float(0)
@@ -225,6 +245,14 @@ o.latitude = float(0)
 o.height = float(0)
 
 x = main(o ,a1,a2,a3)
+y=[]
+for ele in x:
+    y.append([ele.longitude,ele.latitude,ele.height])
+print(y)
+print(y[500])
+print(y[1000])
+print(y[1499])
+d3_points(y)
 '''x是路径的list,每取出一个都要做一次get_GPS（），这样就是原本输入的GPS坐标了，同时要将list顶部的元素移除'''
 '''后面还要有一个投哪个的分支判别，判断是哪个就把哪个的boom方法返回的list“+”到x上'''
 '''当x取尽之后，就让飞机返航'''
